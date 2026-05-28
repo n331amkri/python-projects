90b82fb087de0c63dfd6bea080413fdce24d90ca
n331amkri <neelam.nitw@gmail.com>
2026-05-28 15:43:39 +1000

    initial rock papers scissors game

--- rock-papers-scissors.py
+++ rock-papers-scissors.py
@@ -1,1 +1,22 @@
 import random
+
+def play():
+    print("Welcome to Rock, Paper, Scissors!")
+    user = input("Enter your choice (rock: 'r', paper: 'p', scissors: 's'): ").lower()
+
+    computer = random.choice(['r','p','c'])
+     
+    if(user == computer):
+        return "It's a tie!"
+    
+    else:
+        return check_win(user, computer)
+    
+
+def check_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'):
+        return "You win!"
+    else:
+        return "You lose!"
+        
+print(play())
